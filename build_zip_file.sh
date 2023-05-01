fc51b6bcd8425903529782666e998471d2f4c667
saiprashanthts@gmail.com <saiprashanthts@gmail.com>
2023-04-30 21:25:12 -0400

    added the zip logic

--- build_zip_file.sh
+++ build_zip_file.sh
@@ -1,1 +1,19 @@
-declare -a
+declare -a package_name=("retrive_s3_details" "abc")
+echo 123
+
+echo "removing the build folder"
+rm -rf build
+
+# Installing packages required
+pip install -r requirements.txt -t build
+
+## adding the zip file
+cd build
+zip -r retrive_s3_details.zip .
+
+## adding lambda function
+cd ..
+zip -r retrive_s3_details.zip retrive_s3_details/lambda_function.py
+
+## checking the contents of zip file
+unzip -t retrive_s3_details.zip
