18ff0cd43eca872531db3b484900604943ce6574
saiprashanthts@gmail.com <saiprashanthts@gmail.com>
2023-04-30 22:07:03 -0400

    updated

--- build_zip_file.sh
+++ build_zip_file.sh
@@ -15,8 +15,8 @@ cd build
 zip -r ../retrive_s3_details.zip .
 
 ## adding lambda function
-cd ..
-zip -r retrive_s3_details.zip retrive_s3_details/lambda_function.py
+cd ../retrive_s3_detailse
+zip -r ../retrive_s3_details.zip retrive_s3_details/lambda_function.py
 
 ## checking the contents of zip file
 unzip -t retrive_s3_details.zip
